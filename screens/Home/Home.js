4b1e7e727328fa0568413baf8a5783d99f33a959
Victor-Mutwiri <mutwirilincoln19@gmail.com>
2024-07-12 06:47:51 +0300

    created search component

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,28 +1,19 @@
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import {SafeAreaView} from 'react-native';
 import globalStyle from '../../assets/styles/globalStyle';
 import style from './style';
 import Header from '../../components/Header/Header';
 import Button from '../../components/Button/Button';
 import Tab from '../../components/Tab/Tab';
 import Badge from '../../components/Badge/Badge';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faSearch} from '@fortawesome/free-solid-svg-icons';
+import Search from '../../components/Search/Search';
 
 const Home = () => {
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
-      <View>
-        <Header title={'Azzahri A.'} type={1} />
-        {/* <Button
-          title={'Donate'}
-          onPress={() => {
-            console.log('You just pressed me');
-          }}
-        />
-        <Button title={'Donate'} isDisabled={true} /> */}
-        <Tab title="Highlight" />
-        <Tab title="Highlight" isInactive={true} />
-        <Badge title="Environment" />
-      </View>
+      <Search />
     </SafeAreaView>
   );
 };
