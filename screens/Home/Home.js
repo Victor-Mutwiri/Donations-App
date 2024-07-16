c34b0c4baebc1143292a69dbcd956400316f15cc
Victor-Mutwiri <mutwirilincoln19@gmail.com>
2024-07-16 14:10:19 +0300

    created donations

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
 import globalStyle from '../../assets/styles/globalStyle';
+import {View} from 'react-native';
 import style from './style';
 import Header from '../../components/Header/Header';
 import Button from '../../components/Button/Button';
@@ -9,11 +10,32 @@ import Badge from '../../components/Badge/Badge';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {faSearch} from '@fortawesome/free-solid-svg-icons';
 import Search from '../../components/Search/Search';
+import SingleDonationItem from '../../components/SingleDonationItem/SingleDonationItem';
+import {horizontalScale} from '../../assets/styles/scaling';
 
 const Home = () => {
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
-      <Search />
+      <Search onSearch={value => console.log(value)} />
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          paddingHorizontal: horizontalScale(24),
+        }}>
+        <SingleDonationItem
+          uri="https://img.pixers.pics/pho_wat(s3:700/FO/44/24/64/31/700_FO44246431_ab024cd8251bff09ce9ae6ecd05ec4a8.jpg,525,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,305,650,jpg)/stickers-cactus-cartoon-illustration.jpg.jpg"
+          badgeTitle="Environment"
+          donationTitle="Tree Cactus"
+          price={44}
+        />
+        <SingleDonationItem
+          uri="https://img.pixers.pics/pho_wat(s3:700/FO/44/24/64/31/700_FO44246431_ab024cd8251bff09ce9ae6ecd05ec4a8.jpg,525,700,cms:2018/10/5bd1b6b8d04b8_220x50-watermark.png,over,305,650,jpg)/stickers-cactus-cartoon-illustration.jpg.jpg"
+          badgeTitle="Environment"
+          donationTitle="Tree Cactus"
+          price={44}
+        />
+      </View>
     </SafeAreaView>
   );
 };
